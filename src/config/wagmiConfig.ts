e59719f864d5a4e753d1dede8494e8a860d227e3
0xardy <ardy@wonderland.xyz>
2025-06-03 12:16:36 -0300

    chore: update wagmi config

--- src/config/wagmiConfig.ts
+++ src/config/wagmiConfig.ts
@@ -1,7 +1,7 @@
 import { connectorsForWallets } from '@rainbow-me/rainbowkit';
 import { rainbowWallet, walletConnectWallet, injectedWallet } from '@rainbow-me/rainbowkit/wallets';
 import { createConfig, http, cookieStorage, createStorage } from 'wagmi';
-import { localhost, sepolia } from 'wagmi/chains';
+import { sepolia } from 'wagmi/chains';
 import { getConfig } from '~/config';
 
 const { PROJECT_ID } = getConfig().env;
@@ -28,13 +28,12 @@ const connectors = connectorsForWallets(
 );
 
 export const config = createConfig({
-  chains: [localhost, sepolia],
+  chains: [sepolia],
   ssr: true,
   storage: createStorage({
     storage: cookieStorage,
   }),
   transports: {
-    [localhost.id]: http(),
     [sepolia.id]: http(),
   },
   batch: { multicall: true },
