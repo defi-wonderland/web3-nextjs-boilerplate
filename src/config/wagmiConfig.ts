06bafcb67d0c6e75f7c97803d70531d3597b46f7
th0rOdinson <144699868+th0rOdinson@users.noreply.github.com>
2025-10-15 12:33:35 -0300

    fix: wagmi config best practices

--- src/config/wagmiConfig.ts
+++ src/config/wagmiConfig.ts
@@ -1,6 +1,6 @@
 import { connectorsForWallets } from '@rainbow-me/rainbowkit';
 import { rainbowWallet, walletConnectWallet, injectedWallet } from '@rainbow-me/rainbowkit/wallets';
-import { createConfig, http, cookieStorage, createStorage, type Config } from 'wagmi';
+import { createConfig, http, cookieStorage, createStorage } from 'wagmi';
 import { sepolia } from 'wagmi/chains';
 import { getConfig as getAppConfig } from '~/config';
 
@@ -14,36 +14,30 @@ const getWallets = () => {
   }
 };
 
-let wagmiConfig: Config | undefined;
-
-export const getConfig = () => {
-  if (!wagmiConfig) {
-    const connectors = connectorsForWallets(
-      [
-        {
-          groupName: 'Recommended',
-          wallets: getWallets(),
-        },
-      ],
+export function getConfig() {
+  const connectors = connectorsForWallets(
+    [
       {
-        appName: 'Web3 React boilerplate',
-        projectId: PROJECT_ID,
+        groupName: 'Recommended',
+        wallets: getWallets(),
       },
-    );
+    ],
+    {
+      appName: 'Web3 React boilerplate',
+      projectId: PROJECT_ID,
+    },
+  );
 
-    wagmiConfig = createConfig({
-      chains: [sepolia],
-      ssr: true,
-      storage: createStorage({
-        storage: cookieStorage,
-      }),
-      transports: {
-        [sepolia.id]: http(),
-      },
-      batch: { multicall: true },
-      connectors,
-    });
-  }
-
-  return wagmiConfig;
-};
+  return createConfig({
+    chains: [sepolia],
+    ssr: true,
+    storage: createStorage({
+      storage: cookieStorage,
+    }),
+    transports: {
+      [sepolia.id]: http(),
+    },
+    batch: { multicall: true },
+    connectors,
+  });
+}
